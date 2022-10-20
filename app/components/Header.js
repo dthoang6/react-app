d76045a442d5b22648c80c91bbd4a43392c6533a
Dat Hoang <dthoang6@gmail.com>
2022-10-20 16:40:46 -0700

    conditional homepage content by move up the state and pass as prop to subcomponents

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,10 +3,7 @@ import { Link } from "react-router-dom";
 import HeaderLoggedOut from "./HeaderLoggedOut";
 import HeaderLoggedIn from "./HeaderLoggedIn";
 
-function Header() {
-  //when the page first load, we want to check if data exist in local storage or not
-  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")));
-
+function Header(props) {
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -15,7 +12,7 @@ function Header() {
             React App
           </Link>
         </h4>
-        {loggedIn ? <HeaderLoggedIn setLoggedIn={setLoggedIn} /> : <HeaderLoggedOut setLoggedIn={setLoggedIn} />}
+        {props.loggedIn ? <HeaderLoggedIn setLoggedIn={props.setLoggedIn} /> : <HeaderLoggedOut setLoggedIn={props.setLoggedIn} />}
       </div>
     </header>
   );
