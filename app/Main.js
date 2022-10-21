840f417eee01f2b5dcf275bba717c8f34d0fb0cc
Dat Hoang <dthoang6@gmail.com>
2022-10-21 13:15:10 -0700

    finish section 6, flash message, redirect url, liftup the state

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Axios from "axios";
+Axios.defaults.baseURL = "http://localhost:8080";
 //my components
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -8,14 +10,25 @@ import HomeGuest from "./components/HomeGuest";
 import Home from "./components/Home";
 import About from "./components/About";
 import Terms from "./components/Terms";
+import CreatePost from "./components/CreatePost";
+import ViewSinglePost from "./components/ViewSinglePost";
+import FlashMessage from "./components/FlashMessage";
 
 function Main() {
   const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")));
+  const [flashMessage, setFlashMessage] = useState([]); //array of messages
+  //function so we can pass this add flash message into our CreatePost Component
+  function addFlashMessage(msg) {
+    setFlashMessage(prev => prev.concat(msg)); //concat the message to previous message
+  }
   return (
     <BrowserRouter>
+      <FlashMessage messages={flashMessage} />
       <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
         <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+        <Route path="/post/:id" element={<ViewSinglePost />} />
+        <Route path="create-post" element={<CreatePost addFlashMessage={addFlashMessage} />} />
         <Route path="about-us" element={<About />} />
         <Route path="terms" element={<Terms />} />
       </Routes>
---
+++ app/components/FlashMessage.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+
+function FlashMessage(props) {
+  return (
+    <div className="floating-alerts">
+      //loop through the array of incoming messages
+      {props.messages.map((msg, index) => {
+        return (
+          //in react, whenever looping through a collection and outputting a component for each item in the collection, add key
+          <div key={index} className="alert alert-success text-center floating-alert shadow-sm">
+            {msg}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default FlashMessage;
