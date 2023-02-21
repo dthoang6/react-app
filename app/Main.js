aaaf3bd3e2650e3a895f4e0d54cdc51d317ea5eb
Your Name <dthoang6@gmail.com>
2023-02-21 12:19:44 -0800

    review the workflow for react by using webpack, snippet code, generic component, vsc setting

--- app/Main.js
+++ app/Main.js
@@ -1,22 +1,22 @@
-import React, { useState, useReducer, useEffect } from "react";
-import ReactDOM from "react-dom/client";
-import { useImmerReducer } from "use-immer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Axios from "axios";
-Axios.defaults.baseURL = "http://localhost:8080";
-import StateContext from "./StateContext";
-import DispatchContext from "./DispatchContext";
+import React, { useState, useReducer, useEffect } from "react"
+import ReactDOM from "react-dom/client"
+import { useImmerReducer } from "use-immer"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Axios from "axios"
+Axios.defaults.baseURL = "http://localhost:8080"
+import StateContext from "./StateContext"
+import DispatchContext from "./DispatchContext"
 //my components
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import HomeGuest from "./components/HomeGuest";
-import Home from "./components/Home";
-import About from "./components/About";
-import Terms from "./components/Terms";
-import CreatePost from "./components/CreatePost";
-import ViewSinglePost from "./components/ViewSinglePost";
-import FlashMessage from "./components/FlashMessage";
-import Profile from "./components/Profile";
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Home from "./components/Home"
+import About from "./components/About"
+import Terms from "./components/Terms"
+import CreatePost from "./components/CreatePost"
+import ViewSinglePost from "./components/ViewSinglePost"
+import FlashMessage from "./components/FlashMessage"
+import Profile from "./components/Profile"
 
 function Main() {
   const initialState = {
@@ -28,39 +28,39 @@ function Main() {
       username: localStorage.getItem("complexappUsername"),
       avatar: localStorage.getItem("complexappAvatar")
     }
-  };
+  }
   //only working with state
   function ourReducer(draft, action) {
     switch (action.type) {
       case "login":
-        draft.loggedIn = true;
-        draft.user = action.data;
-        return;
+        draft.loggedIn = true
+        draft.user = action.data
+        return
       case "logout":
-        draft.loggedIn = false;
-        return;
+        draft.loggedIn = false
+        return
       case "flashMessage":
-        draft.flashMessage.push(action.value); //directly modify or mutate this array
-        return;
+        draft.flashMessage.push(action.value) //directly modify or mutate this array
+        return
     }
   }
 
-  const [state, dispatch] = useImmerReducer(ourReducer, initialState); //change from react native useReducer to useimmerReducer
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState) //change from react native useReducer to useimmerReducer
 
   //localStorage --> useEffect(a,b) a is a function, b is a list or an array of dependencies that you want to watch for changes. Any time it changes, the function a will run.
   useEffect(() => {
     if (state.loggedIn) {
       //save data to localStorage
-      localStorage.setItem("complexappToken", state.user.token);
-      localStorage.setItem("complexappUsername", state.user.username);
-      localStorage.setItem("complexappAvatar", state.user.avatar);
+      localStorage.setItem("complexappToken", state.user.token)
+      localStorage.setItem("complexappUsername", state.user.username)
+      localStorage.setItem("complexappAvatar", state.user.avatar)
     } else {
       //remove data from localStorage
-      localStorage.removeItem("complexappToken");
-      localStorage.removeItem("complexappUsername");
-      localStorage.removeItem("complexappAvatar");
+      localStorage.removeItem("complexappToken")
+      localStorage.removeItem("complexappUsername")
+      localStorage.removeItem("complexappAvatar")
     }
-  }, [state.loggedIn]);
+  }, [state.loggedIn])
 
   return (
     <StateContext.Provider value={state}>
@@ -80,13 +80,13 @@ function Main() {
         </BrowserRouter>
       </DispatchContext.Provider>
     </StateContext.Provider>
-  );
+  )
 }
 
-const root = ReactDOM.createRoot(document.querySelector("#app"));
+const root = ReactDOM.createRoot(document.querySelector("#app"))
 
-root.render(<Main />);
+root.render(<Main />)
 
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept()
 }
